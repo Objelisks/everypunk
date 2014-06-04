6d4c25fea0b140491e190df402ef6618b1f11eae
Rather <supertyrian@gmail.com>
2014-06-03 23:24:49 -0700

    bot botting

--- bot.js
+++ bot.js
@@ -1,11 +1,32 @@
+var fs = require('fs');
 var Twitter = require('twitter-js-client');
+var progress = JSON.parse(fs.readFileSync('./progress.json'));
 
-var api = new Twitter.Twitter(JSON.parse(fs.readFileSync('./creds.json')));
+function post(tweet, callback) {
+	var api = new Twitter.Twitter(JSON.parse(fs.readFileSync('./creds.json')));
 
-api.postTweet({
-	"status":"test tweet please ignore punk"
-}, function(error) {
-	console.log(error);
-}, function(success) {
-	console.log('posted tweet');
-});
+	api.postTweet({
+		"status": tweet
+	}, function(error) {
+		console.log(error);
+	}, function(success) {
+		console.log(new Date() + ' posted tweet: ' + tweet);
+		callback();
+	});
+}
+
+function doThing() {
+	var dict = fs.readFileSync('./dict.txt', {encoding: 'utf8'}).split('\r\n');
+	var word = dict[progress.next] + 'punk';
+
+	post(word, function() {
+		progress.lastWord = word;
+		progress.next++;
+		fs.writeFile('./progress.json', JSON.stringify(progress));
+	});
+}
+
+// every 30 minutespunk
+var interval = 1000 * 60 * 30;
+doThing();
+setInterval(doThing, interval);
