a2c564fa3e9b9690145e2ae84e1da095a9b0e70e
Objelisks <supertyrian@gmail.com>
2015-09-18 16:00:02 -0400

    now with 100% more wordnik api

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
+var Swagger = require('swagger-client');
 var Twitter = require('twitter-js-client');
-var progress = JSON.parse(fs.readFileSync('./progress.json'));
+var creds = JSON.parse(fs.readFileSync('./creds.json'));
 
 function post(tweet, callback) {
-	var api = new Twitter.Twitter(JSON.parse(fs.readFileSync('./creds.json')));
+	var api = new Twitter.Twitter(creds.twitter);
+
+    console.log('posting', tweet);
 
 	api.postTweet({
 		"status": tweet
@@ -15,18 +18,32 @@ function post(tweet, callback) {
 	});
 }
 
-function doThing() {
-    // \r\n for windows, \n for linux, mac?
-	var dict = fs.readFileSync('./dict.txt', {encoding: 'utf8'}).split('\n'); // CHANGE LINE ENDINGS
-	var word = dict[progress.next] + 'punk';
+var randomParams = {
+    "hasDictionaryDef": "true",
+    "minCorpusCount": 1,
+    "maxCorpusCount": -1,
+    "minDictionaryCount": 0,
+    "maxDictionaryCount": -1,
+    "minLength": 1,
+    "maxLength": 130
+};
 
-	post(word, function() {
-		progress.lastWord = word;
-		progress.next++;
-		fs.writeFile('./progress.json', JSON.stringify(progress));
-	});
+function doThing() {
+    var wordnik = new Swagger({
+        url: 'http://developer.wordnik.com/v4/resources.json',
+        success: function() {
+            wordnik.words.getRandomWord(randomParams, {responseContentType: 'application/json'}, function(res) {
+                var word = res.obj.word + 'punk';
+                post(word, function() { });
+            });
+        },
+        authorizations: {
+            'apiKey': new Swagger.ApiKeyAuthorization('api_key', creds.wordnik, 'query')
+        }
+    });
+    console.log(wordnik);
 }
 
-var interval = 1000 * 60 * 15;
+var interval = 1000 * 60 * 30;
 doThing();
 setInterval(doThing, interval);
